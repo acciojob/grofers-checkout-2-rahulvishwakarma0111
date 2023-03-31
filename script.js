5c7c3ba2fbeb10f8b39b4da271afa7d7d061d478
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-04-01 01:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 //your code here
-const priceElements = document.querySelectorAll('[data-ns-test="price"]');
+// Get all the elements with data-ns-test="prices"
+const pricesElements = document.querySelectorAll('[data-ns-test="prices"]');
 
-// Check the number of price elements
-if (priceElements.length === 0) {
-  console.error('No price elements found');
-} else {
-  console.log(`Found ${priceElements.length} price elements`);
-
-  // Calculate the total price
-  let totalPrice = 0;
-  priceElements.forEach(priceElement => {
-    totalPrice += parseFloat(priceElement.textContent);
-  });
-
-  // Add the total price to the table
-  const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
-  grandTotalElement.textContent = totalPrice.toFixed(2);
-
-  // Check if the grand total matches the sum of prices
-  const expectedGrandTotal = priceElements.length > 0 ? totalPrice.toFixed(2) : '';
-  if (grandTotalElement.textContent !== expectedGrandTotal) {
-    console.error(`Grand total (${grandTotalElement.textContent}) does not match sum of prices (${expectedGrandTotal})`);
-  } else {
-    console.log(`Grand total (${grandTotalElement.textContent}) matches sum of prices (${expectedGrandTotal})`);
-  }
+// Calculate the sum of all prices
+let total = 0;
+for (let i = 0; i < pricesElements.length; i++) {
+  total += parseFloat(pricesElements[i].innerText);
 }
+
+// Create a new row for the total and add it to the table
+const newRow = document.createElement('tr');
+const totalCell = document.createElement('td');
+totalCell.innerText = `Total: $${total.toFixed(2)}`;
+newRow.appendChild(totalCell);
+document.querySelector('table').appendChild(newRow);
+
+// Set the data-ns-test attribute for the total cell
+totalCell.setAttribute('data-ns-test', 'grandTotal');
